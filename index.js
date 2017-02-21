230fec116b4ff339230daf212e491928d9d6efd7
Joe Crick <joe@bitovi.com>
2017-02-21 09:12:08 +0100

    (master): Add css-only dropdown

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
 import './accordion/accordion.less';
-import TabContent from './tab-content.json';
-
-const doc = document;
-
-doc.querySelector('#dynamic-content').addEventListener('click', event => {
-  const target = event.target;
-  if(target.className === 'tab-header') {
-    const content = target.parentNode.querySelector('.content');
-    if(!content.textContent.trim()) {
-      content.textContent = TabContent[content.getAttribute('data-content-type')];
-    }
-  }
-});
+import './dropdown/dropdown.less';
