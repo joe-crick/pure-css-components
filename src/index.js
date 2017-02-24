41008c45c0d2721017f46a704180f3beb7ee4607
Joe Crick <joe@bitovi.com>
2017-02-24 14:08:23 +0100

    (master): Add preliminary code examples

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import 'purecss/build/base-context-min.css';
 import 'purecss/build/buttons-min.css';
 import './main.less';
+import Clipboard from 'clipboard';
+
+const clipboard = new Clipboard('.copy-btn');
