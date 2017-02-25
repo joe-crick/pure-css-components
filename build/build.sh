8757cb887abbeba93d344f1faa8e012871dbc8d3
Joe Crick <joe@josephcrick.com>
2017-02-25 08:31:16 +0100

    (master): Include dist folder

--- build/build.sh
+++ build/build.sh
@@ -9,7 +9,7 @@ rm -rf dist
 # Write
 for file in ${COMPONENTS}
 do
-  echo "Processing ${file} file..."
+  echo "Processing ${file}..."
   DEST_FILE=${file//src\/components/bundles}
   eval "${COMPILE_DIST} ${file} dist/${DEST_FILE//\.less/.css}"
 done
@@ -17,3 +17,5 @@ done
 eval "${COMPILE_MAP} src/main.less ./main.css"
 eval "${COMPILE_DIST} src/stand-alone-imports.less dist/stand-alone-imports.css"
 eval "${COMPILE_DIST} src/with-pure-imports.less dist/with-pure-imports.css"
+
+tar cvzf dist/pure-components.tar.gz dist
